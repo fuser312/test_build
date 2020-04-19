342cbaa6b7ea7de3be6d02674256d11e553067ab
fuser312 <sherltonGT40000>
2020-04-19 21:26:18 +0530

    Initial commit

---
+++ precache-manifest.8914b2ebdaaea84913f2a86d57fb819e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43697b4efa352e3a8516e615d235f957",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bdae6e9d6defcdbdd51",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "32904294978730a11e40",
+    "url": "/static/js/2.5d8e5333.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d8e5333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bdae6e9d6defcdbdd51",
+    "url": "/static/js/main.281289d0.chunk.js"
+  },
+  {
+    "revision": "759e8bc83204a5cf8777",
+    "url": "/static/js/runtime-main.4a8f8937.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
